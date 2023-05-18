035218e4bbb0f068b3fa975cab4c2b6b47ce5082
Cledilson Filho <cledilsondevoffice@gmail.com>
2023-05-18 17:06:07 -0300

    Iniciando projeto

---
+++ backend/src/main/java/com/devoffice/stdelivery/StdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devoffice.stdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devoffice/stdelivery/StdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devoffice.stdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
